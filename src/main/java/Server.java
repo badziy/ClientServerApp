ba2800ea24435d16ccb7f33e09288b9cc215c017
Badziev Alan <badjity@yandex.ru>
2022-01-22 10:47:07 +0300

    Initial commit

---
+++ src/main/java/Server.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.BindException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        System.out.println("server started");
+        int port = 8080;
+
+        while (true) {
+            ServerSocket serverSocket = new ServerSocket(port);
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            System.out.println("New connection accepted");
+
+            out.println("Введите имя");
+            final String username = in.readLine();
+            out.println("Вы ощущаете себя ребёнком (да/нет)");
+            final String answer = in.readLine();
+            if (answer.equals("да")) {
+                out.println(String.format("Добро пожаловать в детскую зону, %s! Давай играть!", username));
+            } else if (answer.equals("нет")) {
+                out.println(String.format("Добро пожаловать в взрослую зону, %s! Хорошего отдыха или плодотворного рабочего дня!", username));
+            } else {
+                out.println("Ошибка");
+            }
+            throw new BindException("Соединение разорвано");
+        }
+    }
+}
